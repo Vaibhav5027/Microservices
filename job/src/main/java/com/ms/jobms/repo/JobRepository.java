5b77f680a870aa3212470615f1405916f5869e9c
Vaibhav5027 <125240878+Vaibhav5027@users.noreply.github.com>
2024-09-29 10:13:32 +0530

    Delete job/src/main/java/com/ms/jobms directory

--- job/src/main/java/com/ms/jobms/repo/JobRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ms.jobms.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ms.jobms.model.Job;
-
-public interface JobRepository extends JpaRepository<Job, Integer> {
-
-}
